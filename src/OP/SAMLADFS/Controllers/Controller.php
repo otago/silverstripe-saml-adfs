9816a082da6acc45ebad7c62eb0b8d0bb58091a8
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-11-02 13:41:27 +1300

    Got the certs

--- src/OP/SAMLADFS/Controllers/Controller.php
+++ src/OP/SAMLADFS/Controllers/Controller.php
@@ -2,10 +2,13 @@
 
 namespace OP\SAMLADFS\Controllers;
 
+use SilverStripe\Assets\FilenameParsing\HashFileIDHelper;
 use SilverStripe\Core\Environment;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\SAML\Control\SAMLController;
 use SilverStripe\SAML\Helpers\SAMLHelper;
+use SilverStripe\SiteConfig\SiteConfig;
 
 class Controller extends SAMLController
 {
@@ -24,18 +27,35 @@ class Controller extends SAMLController
 
     public static function SP()
     {
+        $siteconfig = DataObject::get_one(SiteConfig::class);
         return [
-            'entityId' => Environment::getEnv("SAMLADFS_SP_ENTITY_ID")
+            'entityId' => Environment::getEnv("SAMLADFS_SP_ENTITY_ID"),
+            'privateKey' => self::getProtectedFilePath($siteconfig->SP_Private_Key()),
+            'x509cert' => self::getProtectedFilePath($siteconfig->SP_X509_Cert())
         ];
     }
 
     public static function IDP()
     {
+        $siteconfig = DataObject::get_one(SiteConfig::class);
         return [
             'entityId' => Environment::getEnv("SAMLADFS_IDP_ENTITY_ID"),
             'singleSignOnService' => Environment::getEnv("SAMLADFS_IDP_SINGLE_SIGNON_SERVICE"),
             'singleLogoutService' => Environment::getEnv("SAMLADFS_IDP_SINGLE_LOGOUT_SERVICE"),
-            'metadata' => Environment::getEnv("SAMLADFS_IDP_METADATA")
+            'metadata' => Environment::getEnv("SAMLADFS_IDP_METADATA"),
+            'x509cert' => self::getProtectedFilePath($siteconfig->IDP_X509_Cert())
         ];
+    }
+
+    public static function getProtectedFilePath($protected_file)
+    {
+        $parent = $protected_file->Parent();
+        $path = '';
+        do {
+            $path = $parent->Name . DIRECTORY_SEPARATOR . $path;
+            $parent = $parent->Parent();
+        } while ($parent->ID);
+        $subfolder = substr($protected_file->getHash(), 0, HashFileIDHelper::HASH_TRUNCATE_LENGTH);
+        return ASSETS_PATH . DIRECTORY_SEPARATOR . '.protected' . DIRECTORY_SEPARATOR . $path . $subfolder . DIRECTORY_SEPARATOR . $protected_file->Name;
     }
 }
