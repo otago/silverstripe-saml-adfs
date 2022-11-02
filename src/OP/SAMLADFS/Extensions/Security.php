3dfa5ebac3d642177e3526b4d7220d8c770b1d86
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-11-02 13:11:31 +1300

    Only use saml adfs if configured

--- src/OP/SAMLADFS/Extensions/Security.php
+++ src/OP/SAMLADFS/Extensions/Security.php
@@ -5,13 +5,25 @@ namespace OP\SAMLADFS\Extensions;
 use SilverStripe\SAML\Helpers\SAMLHelper;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\SiteConfig\SiteConfig;
 
 class Security extends DataExtension
 {
     public function onBeforeSecurityLogin()
     {
-        $request = $this->owner->getRequest();
-        $helper = Injector::inst()->get(SAMLHelper::class);
-        $helper->redirect(null, $request, $request->getURL(true));
+        if ($this->isConfigured()) {
+            $request = $this->owner->getRequest();
+            $helper = Injector::inst()->get(SAMLHelper::class);
+            $helper->redirect(null, $request, $request->getURL(true));
+        }
+    }
+
+    public function isConfigured()
+    {
+        $siteconfig = DataObject::get_one(SiteConfig::class);
+        return $siteconfig->SP_Private_Key()->ID &&
+            $siteconfig->SP_X509_Cert()->ID &&
+            $siteconfig->IDP_X509_Cert()->ID;
     }
 }
